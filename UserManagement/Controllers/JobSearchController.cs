2facca12557c746fa6f0a13c3ed9f54a7a239ac6
santosh maurya <santosh98maurya@hotmail.com>
2022-01-25 14:17:09 +0530

    added Controller

---
+++ UserManagement/Controllers/JobSearchController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserManagement.Controllers.ResponseMapper
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobSearchController : ControllerBase
+    {
+    }
+}
---
+++ UserManagement/Controllers/ProfileController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfileController : ControllerBase
+    {
+    }
+}
